3a55c5d87b53ee490258007df548c5ba98ca23ee
shadow3x3x3 <shadow3x3x3@gmail.com>
2016-07-26 15:21:02 +0800

    add aggregate max and min function

--- ext/array.rb
+++ ext/array.rb
@@ -15,13 +15,52 @@ class Array
   end
 
   # skyline attributes aggregate for array
-  def aggregate(array)
-    aggregate_array = []
+  def aggregate(array, type = 'normal')
     raise "Need Array not #{array.class}"    unless array.class == Array
     raise 'Two Arrays are not the same size' unless size == array.size
+
+    case type
+    when 'normal'
+      return aggregate_normal(array)
+    when 'max'
+      return aggregate_max(array)
+    when 'min'
+      return aggregate_min(array)
+    when 'avg'
+      return aggregate_avg(array)
+    when 'median'
+      return aggregate_median(array)
+    end
+  end
+
+  def aggregate_normal(array)
+    aggregate_array = []
     each_with_index do |attr, index|
       aggregate_array << (attr + array[index]).round(6)
     end
     aggregate_array
   end
+
+  def aggregate_max(array)
+    aggregate_array = []
+    each_with_index do |attr, index|
+      aggregate_array << attr > array[index] ? attr : array[index]
+    end
+    aggregate_array
+  end
+
+  def aggregate_min(array)
+    aggregate_array = []
+    each_with_index do |attr, index|
+      aggregate_array << attr < array[index] ? attr : array[index]
+    end
+    aggregate_array
+  end
+
+  def aggregate_avg(array)
+  end
+
+  def aggregate_median(array)
+  end
+
 end
