67aa02d41b3a6b2a29009f323da57319c06ddc7d
shadow3x3x3 <shadow3x3x3@gmail.com>
2016-07-26 20:14:04 +0800

    refactor Edge with postion_check function

--- structure/edge.rb
+++ structure/edge.rb
@@ -10,13 +10,7 @@ class Edge
   end
 
   def set_subspace(postions)
-    unless postions.class == Array
-      raise ArgumentError, "postions should be an Array"
-    end
-
-    if postions.max > @attrs.size - 1
-      raise ArgumentError, "postions out of range (#{@attrs.size - 1})"
-    end
+    postion_check(postions)
 
     subspace_attrs = []
     postions.sort.each do |p|
@@ -25,7 +19,27 @@ class Edge
     @attrs = subspace_attrs
   end
 
+  def set_max_attrs(postions)
+    postion_check(postions)
+  end
+
+  def set_min_attrs(postions)
+    postion_check(postions)
+  end
+
   def dist
     @attrs.first
+  end
+
+  private
+
+  def postion_check(postions)
+    unless postions.class == Array
+      raise ArgumentError, "postions should be an Array"
+    end
+
+    if postions.max > @attrs.size - 1
+      raise ArgumentError, "postions out of range (#{@attrs.size - 1})"
+    end
   end
 end
