40a22e19ba3f2eb13cde6ec5f4caece9ed13fc0d
shadow3x3x3 <shadow3x3x3@gmail.com>
2016-07-26 17:55:04 +0800

    add subspace attrs setting function and rspec

--- spec/edge_spec.rb
+++ spec/edge_spec.rb
@@ -4,8 +4,9 @@ require_relative '../structure/edge.rb'
 
 
 describe Edge do
+  let(:edge) { Edge.new([0, 3, 4, 1, 2, 3, 4]) }
+
   describe '#initialize' do
-    let(:edge) { Edge.new([0, 3, 4, 1, 2, 3, 4]) }
     it 'id should be 0' do
       expect(edge.id).to eq(0)
     end
@@ -21,6 +22,24 @@ describe Edge do
 
     it 'dist should be 1.0' do
       expect(edge.dist).to eq(1.0)
+    end
+  end
+
+  describe '#set_subspace' do
+    context 'when we set subspace postions' do
+      describe 'set [0, 3]' do
+        it 'attrs should be [1, 4]' do
+          edge.set_subspace([0, 3])
+          expect(edge.attrs).to eq([1, 4])
+        end
+      end
+
+      describe 'set [0, 1, 2]' do
+        it 'attrs should be [1, 2, 3]' do
+          edge.set_subspace([0, 1, 2])
+          expect(edge.attrs).to eq([1, 2, 3])
+        end
+      end
     end
   end
 end
--- structure/edge.rb
+++ structure/edge.rb
@@ -9,6 +9,22 @@ class Edge
     @attrs = attrs
   end
 
+  def set_subspace(postions)
+    unless postions.class == Array
+      raise ArgumentError, "postions should be an Array"
+    end
+
+    if postions.max > @attrs.size - 1
+      raise ArgumentError, "postions out of range (#{@attrs.size - 1})"
+    end
+
+    subspace_attrs = []
+    postions.each do |p|
+      subspace_attrs << @attrs[p]
+    end
+    @attrs = subspace_attrs
+  end
+
   def dist
     @attrs.first
   end
